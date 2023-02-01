841546c1aed5fdddd1d20908d8360887d7da1c2f
Ander <106649207+Andergutobi@users.noreply.github.com>
2023-02-01 01:29:18 +0100

    Ejercicio 5, curso Python, Open Bootcamp.
    Escribe una función que pueda decirte si un año (número entero) es bisiesto o no.

---
+++ Ejercicio_5.py
@@ -0,0 +1,8 @@
+def bisiesto(year):
+    if year %4 == 0 and year & 100 != 0:
+        if year % 400 == 0:
+            print('el año', year,'es bisiesto.')
+    elif year % 4 != 0:
+        print('el año', year, 'no es bisiesto')
+year = int(input('Introduce el año que quieras saber si es bisiesto. '))
+bisiesto(year)
